4239f3a3845643763f97127dd16eb665faa1c611
A-baoYang <rubyyang0615@gmail.com>
2023-03-06 22:26:30 +0800

    Update openai_utils.py

--- openai_utils.py
+++ openai_utils.py
@@ -1,5 +1,6 @@
-from time import sleep
 import configparser
+from time import sleep
+
 import openai
 
 
@@ -22,22 +23,43 @@ class OpenAIAPI:
                     0
                 ]["embedding"]
                 success = True
+
             except Exception as e:
                 print(e)
                 sleep(60)
         return res
 
-    def get_completion(self, prompt, model="text-davinci-003", generation_params=None):
+    def get_text_completion(
+        self, prompt, model="text-davinci-003", generation_params=None
+    ):
         """generation_params (default)
-        - tmperature=0.7
+        - temperature=0.7
         - max_tokens=1024
         """
         if not generation_params:
-            generation_params = {"tmperature": 0.7, "max_tokens": 1024}
+            generation_params = {"temperature": 0.7, "max_tokens": 1024}
         print(f"\nPrompt: {prompt}\nWith params: {generation_params}")
         response = self.openai.Completion.create(
             model=model, prompt=prompt, **generation_params
         )
         res = response["choices"][0]["text"].strip()
         print(f"\nGPT-3 Reply: {res}\n")
+        return res
+
+    def get_chat_completion(
+        self, messages, model="gpt-3.5-turbo", generation_params=None
+    ):
+        """generation_params (default)
+        - temperature=0.7
+        - max_tokens=1024
+        """
+        if not generation_params:
+            generation_params = {"temperature": 0.7, "max_tokens": 1024}
+        print(f"\nMessages: {messages}\nWith params: {generation_params}")
+
+        response = self.openai.ChatCompletion.create(
+            model=model, messages=messages, **generation_params
+        )
+        res = response["choices"][0]["message"]["content"].strip()
+        print(f"\nChatGPT Reply: {res}\n")
         return res
